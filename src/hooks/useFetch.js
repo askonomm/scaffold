5969270da6cb0c2c9ee73f6d3024ad233e25053f
Asko Nõmm <asko@asko.dev>
2024-04-07 19:52:49 +0300

    Improve useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+/**
+ * Fetch data from an API
+ *
+ * @param {RequestInfo | URL} url
+ * @param {RequestInit} options
+ * @returns {{data: unknown, loading: boolean, error: unknown}}
+ */
 export default function useFetch(url, options = {}) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -8,11 +15,17 @@ export default function useFetch(url, options = {}) {
   useEffect(() => {
     setLoading(true);
     fetch(url, options)
-      .then((response) => response.json())
+      .then(async (response) => {
+        return {
+          headers: response.headers,
+          status: response.status,
+          json: await response.json(),
+        };
+      })
       .then((data) => setData(data))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
-  }, []);
+  }, [url]);
 
   return { data, loading, error };
 }

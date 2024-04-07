d262b29fab9a2eaed9695311b9ab75ca862e7502
Asko Nõmm <asko@asko.dev>
2024-04-07 19:52:58 +0300

    Improve useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -9,7 +9,7 @@ import { useEffect, useState } from "react";
  */
 export default function useFetch(url, options = {}) {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
